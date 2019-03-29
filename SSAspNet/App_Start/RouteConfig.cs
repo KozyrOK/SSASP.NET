873cd221285cf019f40307852e86e771e4f5e041
Artem Kozyr <Artem Kozyr@DESKTOP-MMVGTCB>
2019-03-30 00:48:28 +0200

    29-03-2019

--- SSAspNet/App_Start/RouteConfig.cs
+++ SSAspNet/App_Start/RouteConfig.cs
@@ -12,7 +12,7 @@ namespace SSAspNet
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "MainController", action = "MainMenu", id = UrlParameter.Optional }
+                defaults: new { controller = "Main", action = "MainMenu", id = UrlParameter.Optional }
             );
         }
     }
